86c6d9d28c79baf757814bb6de9876e42770a699
Patrick <patrick.bryant@live.com>
2020-04-06 18:00:29 +0200

    formatting data due to icl

--- simulations/icl_model/model.sh
+++ simulations/icl_model/model.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -l
 
+DATADIR=/home/patrick/COVID19.github.io/simulations/icl_model/data/
 OUTDIR=/home/patrick/COVID19.github.io/docs/assets/
 
-/home/patrick/COVID19.github.io/simulations/icl_model.py --outdir $OUTDIR
+/home/patrick/COVID19.github.io/simulations/icl_model/icl_model.py --datadir $DATADIR --outdir $OUTDIR
