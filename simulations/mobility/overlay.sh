5bef108ce90fb4d6d06e3b3957e11c742ff71ec1
Patrick <patrick.bryant@live.com>
2020-04-12 18:27:43 +0200

    R0 2.79

--- simulations/mobility/overlay.sh
+++ simulations/mobility/overlay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
 DATADIR=/home/patrick/COVID19.github.io/simulations/mobility/data/
-OUTDIR=/home/patrick/COVID19.github.io/simulations/mobility/model_output/
+OUTDIR=/home/patrick/COVID19.github.io/simulations/mobility/model_output/R0_2_79/
 
 /home/patrick/COVID19.github.io/simulations/mobility/mobility_intervention_overlay.py --datadir $DATADIR --outdir $OUTDIR
