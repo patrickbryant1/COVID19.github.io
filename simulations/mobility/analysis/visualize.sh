55056323d837428815b7bf6f664bcda86d450c45
Patrick <patrick.bryant@live.com>
2020-04-16 14:53:40 +0200

    created short dates for plotting

--- simulations/mobility/analysis/visualize.sh
+++ simulations/mobility/analysis/visualize.sh
@@ -5,6 +5,6 @@ OUTDIR=/home/patrick/COVID19.github.io/simulations/mobility/model_output/R0_2_79
 COUNTRIES="Austria,Belgium,Denmark,France,Germany,Italy,Norway,Spain,Sweden,Switzerland,United_Kingdom" #Make sure these are in the same order as when simulating!
 DTS=84
 #Visualize model output
-/home/patrick/COVID19.github.io/simulations/mobility/visualize_model_output.py --datadir $DATADIR --countries $COUNTRIES --days_to_simulate $DTS --outdir $OUTDIR
+/home/patrick/COVID19.github.io/simulations/mobility/analysis/visualize_model_output.py --datadir $DATADIR --countries $COUNTRIES --days_to_simulate $DTS --outdir $OUTDIR
 #Overlay mobility and intervention
 #/home/patrick/COVID19.github.io/simulations/mobility/mobility_intervention_overlay.py --datadir $DATADIR --outdir $OUTDIR
