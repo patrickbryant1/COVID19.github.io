4469cf2e09904619490c438d7588a9bfa5d97877
Patrick <pbryant@ae18a.scilifelab.se>
2020-04-29 16:20:33 +0200

    dev

--- simulations/mobility/dev/dev.sh
+++ simulations/mobility/dev/dev.sh
@@ -5,5 +5,5 @@ COUNTRIES="Sweden"
 STAN_MODEL=./mobility_population.stan
 DTS=69 #Days to simulate
 OUTDIR=../model_output/R0_2_79/dev/
-ED=2020-04-19 #End date, up to which to include data (different depending on forecast)
+ED=2020-03-29 #End date, up to which to include data (different depending on forecast)
 ./dev_model.py --datadir $DATADIR --countries $COUNTRIES --stan_model $STAN_MODEL --days_to_simulate $DTS --end_date $ED --outdir $OUTDIR
