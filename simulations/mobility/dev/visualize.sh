47d841b81795b7e510607f51e87059936e861062
Patrick <patrick.bryant@live.com>
2020-04-29 22:37:14 +0200

    plotting dev

--- simulations/mobility/dev/visualize.sh
+++ simulations/mobility/dev/visualize.sh
@@ -3,7 +3,7 @@
 DATADIR=../data/
 OUTDIR=../model_output/R0_2_79/dev/
 COUNTRIES="Sweden" #Make sure these are in the same order as when simulating!
-DTS=83
+DTS=69
 SD=$OUTDIR'/plots/short_dates.csv'
 #Visualize model output
 ./visualize_model_output.py --datadir $DATADIR --countries $COUNTRIES --days_to_simulate $DTS --short_dates $SD --outdir $OUTDIR
