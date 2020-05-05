fc72deba92181146d964896bb20f1de10d6d2d60
Arne Elofsson (ae18c) <arne@bioinfo.se>
2020-05-05 23:03:35 +0200

    By syncing

--- simulations/mobility/analysis/visualize_SE.sh
+++ simulations/mobility/analysis/visualize_SE.sh
@@ -15,7 +15,7 @@ do
     for ED in 2020-03-23 2020-04-01 2020-04-07 2020-04-15  # 2020-05-01
     do
 	OUTDIR=/home/arnee/git/COVID19.github.io/simulations/mobility/Sweden/${ED}-${DTS}/
-	if [ -d $OUTDIR ]
+	if [ -s ${OUTDIR}/summary.csv ]
 	then
 	    mkdir  $OUTDIR/plots
 	    python3 /home/arnee/git/COVID19.github.io/simulations/mobility/analysis/visualize_model_output_SE.py --datadir $DATADIR --countries $COUNTRIES --days_to_simulate $DTS --short_dates $SD --outdir $OUTDIR
