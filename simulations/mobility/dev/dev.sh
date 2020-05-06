7348dec977bd8282dabc2cfc0c5680f47558a3d0
Patrick <patrick.bryant@live.com>
2020-05-06 09:21:12 +0200

    modelling countries together

--- simulations/mobility/dev/dev.sh
+++ simulations/mobility/dev/dev.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -l
 
 DATADIR=../data/
-COUNTRIES="Sweden" #Denmark,Norway,
+COUNTRIES="Denmark,Norway,Sweden"
 STAN_MODEL=./mobility_herd.stan
 OUTDIR=../model_output/R0_2_79/dev/
-ED=2020-04-29 #End date, up to which to include data (different depending on forecast)
-./dev_model.py --datadir $DATADIR --country $COUNTRIES --stan_model $STAN_MODEL --end_date $ED --outdir $OUTDIR
+ED=2020-05-05 #End date, up to which to include data (different depending on forecast)
+DTS=106
+./dev_model.py --datadir $DATADIR --countries $COUNTRIES --stan_model $STAN_MODEL --days_to_simulate $DTS --end_date $ED --outdir $OUTDIR
