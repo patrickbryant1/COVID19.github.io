d089ac237f018774d19502609e70e59f1dbb6ad3
Patrick <patrick.bryant@live.com>
2020-05-18 13:54:42 +0200

    added plots

---
+++ docs/R_and_mobility.md
@@ -0,0 +1,16 @@
+---
+layout: page
+title: Correlating R0 changes with mobility changes
+---
+
+
+## Correlation betwen R0 and the mobility data for different time delays
+
+![markers](./assets/mobility_markers.png)
+![countries_1](./assets/countries_1.png)
+![countries_1](./assets/countries_2.png)
+![countries_1](./assets/countries_3.png)
+![countries_1](./assets/countries_4.png)
+![countries_1](./assets/countries_5.png)
+![countries_1](./assets/countries_6.png)
+![countries_1](./assets/countries_7.png)
--- simulations/mobility_signal/construct_signals.py
+++ simulations/mobility_signal/construct_signals.py
@@ -43,76 +43,85 @@ def construct_signals(R_estimates, epidemic_data, mobility_data, outdir):
                            'workplaces_percent_change_from_baseline',
                            'residential_percent_change_from_baseline']
         #Get unique countries
-        #countries = epidemic_data['countriesAndTerritories'].unique()
-        countries = ['Italy']
+        countries = epidemic_data['countriesAndTerritories'].unique()
         for country in countries:
-            country_R = pd.read_csv(R_estimates+country+'_R_estimate.csv')
-            #Fix datetime
-            country_R['date'] = pd.to_datetime(country_R['date'], format='%d/%m/%Y')
-            #Get country epidemic data
-            country_epidemic_data = epidemic_data[epidemic_data['countriesAndTerritories']==country]
-            #Sort on date
-            country_epidemic_data = country_epidemic_data.sort_values(by='dateRep')
-            #Reset index
-            country_epidemic_data = country_epidemic_data.reset_index()
-            #Get data for day >= t_c, where t_c is the day where 80 % of the max death count has been reached
-            death_80 = max(country_epidemic_data['deaths'])*0.8
-            signal_start = min(country_epidemic_data[country_epidemic_data['deaths']>=death_80].index)
-            country_epidemic_data = country_epidemic_data.loc[signal_start:]
-            #Merge dfs
-            country_signal_data = country_epidemic_data.merge(country_R, left_on = 'dateRep', right_on ='date', how = 'left')
+            try:
+                country_R = pd.read_csv(R_estimates+country+'_R_estimate.csv')
+                #Fix datetime
+                country_R['date'] = pd.to_datetime(country_R['date'], format='%d/%m/%Y')
+                #Get country epidemic data
+                country_epidemic_data = epidemic_data[epidemic_data['countriesAndTerritories']==country]
+                #Sort on date
+                country_epidemic_data = country_epidemic_data.sort_values(by='dateRep')
+                #Reset index
+                country_epidemic_data = country_epidemic_data.reset_index()
+                #Get data for day >= t_c, where t_c is the day where 80 % of the max death count has been reached
+                death_80 = max(country_epidemic_data['deaths'])*0.8
+                signal_start = min(country_epidemic_data[country_epidemic_data['deaths']>=death_80].index)
+                country_epidemic_data = country_epidemic_data.loc[signal_start:]
+                #Merge dfs
+                country_signal_data = country_epidemic_data.merge(country_R, left_on = 'dateRep', right_on ='date', how = 'left')
 
-            #Mobility data from Google
-            country_mobility_data = mobility_data[mobility_data['country_region']==country]
-            #Get whole country - no subregion
-            country_mobility_data =  country_mobility_data[country_mobility_data['sub_region_1'].isna()]
-            #Merge
-            country_signal_data = country_signal_data.merge(country_mobility_data, left_on = 'dateRep', right_on ='date', how = 'left')
+                #Mobility data from Google
+                country_mobility_data = mobility_data[mobility_data['country_region']==country]
+                #Get whole country - no subregion
+                country_mobility_data =  country_mobility_data[country_mobility_data['sub_region_1'].isna()]
+                #Merge
+                country_signal_data = country_signal_data.merge(country_mobility_data, left_on = 'dateRep', right_on ='date', how = 'left')
 
-            #Construct signals
-            country_signal_data['R_signal'] = 0
-            country_signal_data['retail_signal'] = 0
-            country_signal_data['grocery_signal'] = 0
-            country_signal_data['transit_signal'] = 0
-            country_signal_data['workplaces_signal'] = 0
-            country_signal_data['residential_signal'] = 0
-            #Look at the smoothing
-            #compare_smoothing(country_signal_data, outdir)
-            #Smooth
-            country_signal_data['Median(R)'] = savgol_filter(country_signal_data['Median(R)'], 5,3)
-            country_signal_data['retail_and_recreation_percent_change_from_baseline'] = savgol_filter(country_signal_data['retail_and_recreation_percent_change_from_baseline'], 7,3)
-            country_signal_data['grocery_and_pharmacy_percent_change_from_baseline'] = savgol_filter(country_signal_data['grocery_and_pharmacy_percent_change_from_baseline'], 7,3)
-            country_signal_data['transit_stations_percent_change_from_baseline'] = savgol_filter(country_signal_data['transit_stations_percent_change_from_baseline'], 7,3)
-            country_signal_data['workplaces_percent_change_from_baseline'] = savgol_filter(country_signal_data['workplaces_percent_change_from_baseline'], 7,3)
-            country_signal_data['residential_percent_change_from_baseline'] = savgol_filter(country_signal_data['residential_percent_change_from_baseline'], 7,3)
-            #Calculate signals
-            for i in range(len(country_signal_data)-1): #loop through data to construct signal
-                row_i = country_signal_data.loc[i]
-                row_i_1 = country_signal_data.loc[i+1]
-                country_signal_data.loc[i+1,'R_signal']=row_i_1['Median(R)']-row_i['Median(R)']
-                country_signal_data.loc[i+1,'retail_signal']=row_i_1['retail_and_recreation_percent_change_from_baseline']-row_i['retail_and_recreation_percent_change_from_baseline']
-                country_signal_data.loc[i+1,'grocery_signal']=row_i_1['grocery_and_pharmacy_percent_change_from_baseline']-row_i['grocery_and_pharmacy_percent_change_from_baseline']
-                country_signal_data.loc[i+1,'transit_signal']=row_i_1['transit_stations_percent_change_from_baseline']-row_i['transit_stations_percent_change_from_baseline']
-                country_signal_data.loc[i+1,'workplaces_signal']=row_i_1['workplaces_percent_change_from_baseline']-row_i['workplaces_percent_change_from_baseline']
-                country_signal_data.loc[i+1,'residential_signal']=row_i_1['residential_percent_change_from_baseline']-row_i['residential_percent_change_from_baseline']
+                #Construct signals
+                country_signal_data['R_signal'] = 0
+                country_signal_data['retail_signal'] = 0
+                country_signal_data['grocery_signal'] = 0
+                country_signal_data['transit_signal'] = 0
+                country_signal_data['workplaces_signal'] = 0
+                country_signal_data['residential_signal'] = 0
+                #Look at the smoothing
+                #compare_smoothing(country_signal_data, outdir)
+                #Smooth
+                if len(country_signal_data)<=7:
+                    print(country, len(country_signal_data))
+                    continue
+                country_signal_data['Median(R)'] = savgol_filter(country_signal_data['Median(R)'], 7,3)
+                country_signal_data['retail_and_recreation_percent_change_from_baseline'] = savgol_filter(country_signal_data['retail_and_recreation_percent_change_from_baseline'], 7,3)
+                country_signal_data['grocery_and_pharmacy_percent_change_from_baseline'] = savgol_filter(country_signal_data['grocery_and_pharmacy_percent_change_from_baseline'], 7,3)
+                country_signal_data['transit_stations_percent_change_from_baseline'] = savgol_filter(country_signal_data['transit_stations_percent_change_from_baseline'], 7,3)
+                country_signal_data['workplaces_percent_change_from_baseline'] = savgol_filter(country_signal_data['workplaces_percent_change_from_baseline'], 7,3)
+                country_signal_data['residential_percent_change_from_baseline'] = savgol_filter(country_signal_data['residential_percent_change_from_baseline'], 7,3)
+                #Calculate signals
+                for i in range(len(country_signal_data)-1): #loop through data to construct signal
+                    row_i = country_signal_data.loc[i]
+                    row_i_1 = country_signal_data.loc[i+1]
+                    country_signal_data.loc[i+1,'R_signal']=row_i_1['Median(R)']-row_i['Median(R)']
+                    country_signal_data.loc[i+1,'retail_signal']=row_i_1['retail_and_recreation_percent_change_from_baseline']-row_i['retail_and_recreation_percent_change_from_baseline']
+                    country_signal_data.loc[i+1,'grocery_signal']=row_i_1['grocery_and_pharmacy_percent_change_from_baseline']-row_i['grocery_and_pharmacy_percent_change_from_baseline']
+                    country_signal_data.loc[i+1,'transit_signal']=row_i_1['transit_stations_percent_change_from_baseline']-row_i['transit_stations_percent_change_from_baseline']
+                    country_signal_data.loc[i+1,'workplaces_signal']=row_i_1['workplaces_percent_change_from_baseline']-row_i['workplaces_percent_change_from_baseline']
+                    country_signal_data.loc[i+1,'residential_signal']=row_i_1['residential_percent_change_from_baseline']-row_i['residential_percent_change_from_baseline']
 
-            #Make an array
-            signal_array = np.zeros((6,len(country_signal_data)))
-            signal_array[0,:]=country_signal_data['R_signal']
-            signal_array[1,:]=country_signal_data['retail_signal']
-            signal_array[2,:]=country_signal_data['grocery_signal']
-            signal_array[3,:]=country_signal_data['transit_signal']
-            signal_array[4,:]=country_signal_data['workplaces_signal']
-            signal_array[5,:]=country_signal_data['residential_signal']
-            correlate_signals(signal_array)
+                #Make an array
+                signal_array = np.zeros((6,len(country_signal_data)))
+                signal_array[0,:]=country_signal_data['R_signal']
+                signal_array[1,:]=country_signal_data['retail_signal']
+                signal_array[2,:]=country_signal_data['grocery_signal']
+                signal_array[3,:]=country_signal_data['transit_signal']
+                signal_array[4,:]=country_signal_data['workplaces_signal']
+                signal_array[5,:]=country_signal_data['residential_signal']
+                #Get pearsonr for different time delays in mobility response
+                C_c = correlate_signals(signal_array)
+                #Plot
+                plot_correlations(C_c, outdir, country)
+            except:
+                continue
 
 def correlate_signals(signal_array):
-    '''
+    '''Correlate the R values with the mobility data
+    using different time delays (s)
     '''
     s_max = signal_array.shape[1]
-    C_c = np.zeros((5,s_max-1)) #Save correaltions btw signals for different s
+    C_c = np.zeros((5,s_max-2)) #Save correaltions btw signals for different s
     #Loop through all s and calculate correlations
-    for s in range(s_max-1): #s is the number of future days to correlate the mobility data over
+    for s in range(s_max-2): #s is the number of future days to correlate the mobility data over
         for m in range(1,6):#all mobility data
 
             if s == 0:
@@ -121,8 +130,25 @@ def correlate_signals(signal_array):
                 r,p = pearsonr(signal_array[0,:-s],signal_array[m,s:])
             #Assign correlation
             C_c[m-1,s]=r
+    return C_c
 
-    pdb.set_trace()
+def plot_correlations(C_c, outdir, country):
+    '''Plot the correlation (PearsonR) btw the R values
+    and different time delays (s)
+    '''
+    keys = ['retail and recreation', 'grocery and pharmacy',
+            'transit stations','workplaces','residential']
+    colors = ['tab:red','tab:purple','tab:pink','tab:olive','tab:cyan']
+    fig, ax = plt.subplots(figsize=(6/2.54, 6/2.54))
+    for i in range(5):
+        ax.plot(range(C_c.shape[1]),C_c[i,:], label=keys[i], color = colors[i], linewidth = 1.0)
+    ax.set_title(country)
+    ax.set_xlabel('mobility time delay (days)')
+    ax.set_ylim([-1,1])
+    ax.set_ylabel('Pearson R')
+    fig.tight_layout()
+    fig.savefig(outdir+country+'.png',  format='png')
+
 def compare_smoothing(country_signal_data, outdir):
     '''Compare different kinds of smoothing
     '''
@@ -140,10 +166,12 @@ def compare_smoothing(country_signal_data, outdir):
         ax.plot(range(len(country_signal_data)),savgol_smoothing, label='Savgol smoothing')
         ax.set_title(key)
         plt.legend()
-        fig.savefig(outdir+key+'.png',  format='png', dpi=300)
+        fig.savefig(outdir+'smoothing/'+key+'.png',  format='png', dpi=300)
 
 
 #####MAIN#####
+#Set font size
+matplotlib.rcParams.update({'font.size': 9})
 args = parser.parse_args()
 R_estimates = args.R_estimates[0]
 epidemic_data = pd.read_csv(args.epidemic_data[0])
@@ -152,3 +180,15 @@ outdir = args.outdir[0]
 
 #Construct signals
 construct_signals(R_estimates, epidemic_data, mobility_data, outdir)
+
+#Mobility markers
+covariate_colors = {'retail and recreation':'tab:red','grocery and pharmacy':'tab:purple', 'transit stations':'tab:pink','workplace':'tab:olive','residential':'tab:cyan'}
+fig, ax = plt.subplots(figsize=(6/2.54,2.25/2.54))
+i=5
+for cov in covariate_colors:
+    ax.plot([1,1.8],[i]*2, color = covariate_colors[cov], linewidth=4)
+    ax.text(2.001,i,cov)
+    i-=1
+ax.set_xlim([0.999,3.5])
+ax.axis('off')
+fig.savefig(outdir+'mobility_markers.png', format = 'png')
