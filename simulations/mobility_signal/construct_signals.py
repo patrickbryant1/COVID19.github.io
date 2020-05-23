9848a850c03b1cbf82e26b2ee1a30eb51fe157f4
Patrick <patrick.bryant@live.com>
2020-05-23 15:04:43 +0200

    checking correlations

--- simulations/mobility_signal/construct_signals.py
+++ simulations/mobility_signal/construct_signals.py
@@ -26,16 +26,18 @@ parser.add_argument('--mobility_data', nargs=1, type= str, default=sys.stdin, he
 parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'Path to outdir.')
 
 ###FUNCTIONS###
-def construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t, outname):
+def construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t, outname, days_to_include):
         '''Read in and format all data needed for the signal correlation analysis
         '''
 
         #Convert epidemic data to datetime
         epidemic_data['dateRep'] = pd.to_datetime(epidemic_data['dateRep'], format='%d/%m/%Y')
+        #Rename date to date
+        epidemic_data = epidemic_data.rename(columns={'dateRep':'date'})
         #Convert mobility data to datetime
         mobility_data['date'] = pd.to_datetime(mobility_data['date'], format='%Y/%m/%d')
         #Get epidemic data due to last mobility date, 2020-05-09
-        epidemic_data = epidemic_data[epidemic_data['dateRep']<=max(mobility_data['date'])]
+        epidemic_data = epidemic_data[epidemic_data['date']<=max(mobility_data['date'])]
         #Mobility key conversions
         key_conversions = {'United_States_of_America':'United_States'}
 
@@ -53,6 +55,10 @@ def construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t
         C_R_delay_all = []
         #Save start dates for epidemic data
         start_dates = []
+        #days before 80 % mark
+        days_before_80 = []
+        #days after 80 % mark
+        days_after_80 = []
         #Get unique countries
         countries = epidemic_data['countriesAndTerritories'].unique()
         for country in countries:
@@ -68,7 +74,7 @@ def construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t
             #Get country epidemic data
             country_epidemic_data = epidemic_data[epidemic_data['countriesAndTerritories']==country]
             #Sort on date
-            country_epidemic_data = country_epidemic_data.sort_values(by='dateRep')
+            country_epidemic_data = country_epidemic_data.sort_values(by='date')
             #Reset index
             country_epidemic_data = country_epidemic_data.reset_index()
             #Get data for day >= t_c, where t_c is the day where 80 % of the max death count has been reached
@@ -76,12 +82,12 @@ def construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t
             signal_start = min(country_epidemic_data[country_epidemic_data['deaths']>=death_t].index)
             if above_t == True:
                 country_epidemic_data = country_epidemic_data.loc[signal_start:]
-                start_dates.append(country_epidemic_data.loc[signal_start,'dateRep'])
+                start_dates.append(country_epidemic_data.loc[signal_start,'date'])
                 #Merge dfs
-                country_signal_data = country_epidemic_data.merge(country_R, left_on = 'dateRep', right_on ='date', how = 'left')
+                country_signal_data = country_epidemic_data.merge(country_R, left_on = 'date', right_on ='date', how = 'left')
             else:
                 #Merge dfs
-                country_signal_data = country_R.merge(country_epidemic_data, left_on = 'date', right_on ='dateRep', how = 'left')
+                country_signal_data = country_R.merge(country_epidemic_data, left_on = 'date', right_on ='date', how = 'left')
 
             #Mobility data from Google
             if country in key_conversions.keys():
@@ -97,10 +103,14 @@ def construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t
                 print('No mobility data for', country)
                 continue
             #Merge
-            country_signal_data = country_signal_data.merge(country_mobility_data, left_on = 'date', right_on ='date', how = 'left')
+            #Merge to the shortest one
+            if len(country_signal_data)>len(country_mobility_data):
+                country_signal_data = country_mobility_data.merge(country_signal_data, left_on = 'date', right_on ='date', how = 'left')
+            else:
+                country_signal_data = country_signal_data.merge(country_mobility_data, left_on = 'date', right_on ='date', how = 'left')
 
             #Smooth
-            if len(country_signal_data)<=21:
+            if len(country_signal_data)<days_to_include:
                 print(country, len(country_signal_data))
                 continue
             country_signal_data['Median(R)'] = savgol_filter(country_signal_data['Median(R)'], 7,3)
@@ -125,7 +135,19 @@ def construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t
             #Check NaNs
             if len(signal_array[np.isnan(signal_array)])>1:
                 print(country, 'contains NaNs')
+                #pdb.set_trace()
                 continue
+            #Reset index
+            country_signal_data = country_signal_data.reset_index()
+
+            if above_t == False:
+                #Append number of days before and after 80% threshold
+                try:
+                    signal_start = min(country_signal_data[country_signal_data['deaths']>=death_t].index)
+                    days_before_80.append(signal_start)
+                    days_after_80.append(len(country_signal_data)-signal_start)
+                except:
+                    pdb.set_trace()
             #Get pearsonr for different time delays in mobility response
             C_mob_delay, C_R_delay = corr_signals(signal_array)
             C_mob_delay_all.append(C_mob_delay)#Save
@@ -134,13 +156,22 @@ def construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t
             #plot_corr(C_mob_delay, C_R_delay, outdir, country, 'Pearson R')
             #For montage script
             fetched_countries.append(country)
+            #Sanity check - see origin of all corr >0.5
+            pos_above_05 = np.where(C_mob_delay>0.5)
+            pos_above_05_i = pos_above_05[0][0] #take first
+            pos_above_05_j = pos_above_05[1][np.where(C_mob_delay[pos_above_05]==max(C_mob_delay[pos_above_05]))]
+            pos_above_05_R=signal_array[0,:pos_above_05_j]
+            pos_above_05_mob=signal_array[pos_above_05_i,pos_above_05_j]
+            pdb.set_trace()
+            neg_above_05 = np.where(C_R_delay>0.5)
+        pdb.set_trace()
         #Plot all countries in overlap per mobility category
-        days_to_include=21
         plot_corr_all_countries(C_mob_delay_all, C_R_delay_all, 'Pearson R', outdir, days_to_include, outname)
 
         #Write montage script
         write_montage(fetched_countries, outdir)
 
+def sanity_check(C_mob_delay, C_R_delay, signal_array)
 def corr_signals(signal_array):
     '''Analyze the correlation of the R values with the mobility data
     using different time delays (s)
@@ -183,15 +214,16 @@ def plot_corr_all_countries(C_mob_delay_all, C_R_delay_all, ylabel, outdir, days
                 continue
             else:
                 plotted_countries +=1
+                #Save all country correltions with delays from 0 to days_to_include
                 all_countries_x.extend(np.arange(days_to_include))
                 all_countries_y.extend(C_mob_delay_all[j][i,:days_to_include])
-                all_countries_x.extend(-np.arange(days_to_include))
+                all_countries_x.extend(-np.arange(days_to_include)) #The R delay is the negative mobility delay
                 all_countries_y.extend(C_R_delay_all[j][i,:days_to_include])
-            sns.kdeplot(all_countries_x,all_countries_y, shade = True, cmap = colors[i])
+        sns.kdeplot(all_countries_x,all_countries_y, shade = True, cmap = colors[i])
         ax.set_title(keys[i])
         ax.set_xlabel('mobility time delay (days)')
         ax.set_ylabel(ylabel)
-
+        ax.set_xlim([-days_to_include-2, days_to_include+2])
         fig.tight_layout()
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
@@ -280,10 +312,12 @@ mobility_data = pd.read_csv(args.mobility_data[0])
 outdir = args.outdir[0]
 
 #Construct signals
-#above_t = False #get data above threshold or not
-#outname='_mobility_delay_all_whole_epi.png'
-#construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t, outname)
-#print('Whole plotted')
-above_t = True #get data above threshold or not
-outname='_mobility_delay_all_above_80.png'
-construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t, outname)
+above_t = False #get data above threshold or not
+outname='_mobility_delay_all_whole_epi.png'
+days_to_include=28
+construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t, outname, days_to_include)
+print('Whole plotted')
+#above_t = True #get data above threshold or not
+#outname='_mobility_delay_all_above_80.png'
+#construct_signals(R_estimates, epidemic_data, mobility_data, outdir, above_t, outname, days_to_include)
+print('Above 80 plotted')
