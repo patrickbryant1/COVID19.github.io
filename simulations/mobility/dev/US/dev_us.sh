dbc08d3ec5311ba4001b4166ac75884e83717b7d
Patrick <patrick.bryant@live.com>
2020-06-15 09:06:56 +0200

    trying another R distribution

--- simulations/mobility/dev/US/dev_us.sh
+++ simulations/mobility/dev/US/dev_us.sh
@@ -2,7 +2,7 @@
 
 DEATHS=/home/patrick/COVID19.github.io/simulations/mobility/data/US/us_deaths.csv
 MOBILITY_DATA=/home/patrick/COVID19.github.io/simulations/mobility/data/Global_Mobility_Report.csv
-STAN_MODEL=./mobility.stan
+STAN_MODEL=./mobility_dispersion.stan
 OUTDIR=../model_output/R0_2_79/dev/
 DTS=112 #Days to simulate
 ./dev_us.py --us_deaths $DEATHS --mobility_data $MOBILITY_DATA --stan_model $STAN_MODEL --days_to_simulate $DTS --outdir $OUTDIR
