86219a9a022e0397a147c4a1947a117a43423ccf
Patrick <patrick.bryant@live.com>
2020-06-26 15:13:44 +0200

    trting to normalize

--- simulations/death_factors/eda/analyze_death.py
+++ simulations/death_factors/eda/analyze_death.py
@@ -23,16 +23,19 @@ parser.add_argument('--sex_eth_age_data', nargs=1, type= str, default=sys.stdin,
 parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'Path to outdir.')
 
 ###FUNCTIONS###
-def format_ethnicity(sex_eth_age_data):
+def format_age_per_ethnicity(sex_eth_age_data):
     '''Extract ethnicity data per state
     '''
     extracted_data = pd.DataFrame()
     sexes = {0:'Total', 1:'Male', 2:'Female'}
+    #Use only total
+    sex_eth_age_data = sex_eth_age_data[sex_eth_age_data['SEX']==0]
     origins = {1:'Non-Hispanic',2:'Hispanic'}
-    ethnicities = {1:'White', 2:'Black',3:'American Indian or Alaska Native',4:'Asian',5:'Native Hawaiian and Other Pacific Islander',6:'More than one race'}
+    ethnicities = {1:'White', 2:'Black',3:'American Indian or Alaska Native',4:'Asian',5:'Native Hawaiian or Other Pacific Islander',6:'More than one race'}
     #Combining origin 1 and ethnicity gives Non-Hispanic + ethinicity (same used in COVID reportings)
     #AGE is single-year of age (0, 1, 2,... 84, 85+ years)
-    age_groups = {0:'Under 1 year',4:'1-4 years',14:'5-14 years',24:'15-24 years',34:'25-34 years',44:'35-44 years',54:'45-54 years', 64:'55-64 yeaers', 74:'65-74 years',84:'75-84 years'}
+    age_groups = {0:'Under 1 year',4:'1-4 years',14:'5-14 years',24:'15-24 years',34:'25-34 years',44:'35-44 years',54:'45-54 years', 64:'55-64 years', 74:'65-74 years',84:'75-84 years', 85:'85 years and over'}
+    age_index = [1,4,14,24,34,44,54,64,74,84]
     #Assign columns
     extracted_data['State'] = ''
     extracted_data['Ethnicity'] = ''
@@ -43,34 +46,87 @@ def format_ethnicity(sex_eth_age_data):
         per_state = sex_eth_age_data[sex_eth_age_data['NAME']==state]
         #Loop through origins
         hispanic_state = per_state[per_state['ORIGIN']==2]
+        vals = []
+        vals.append(state)
+        vals.append('Hispanic or Latino')
+        age_counts = np.zeros(len(age_groups))
+
+        #All the ethnicities in Hispanic should be summed
         for eth in ethnicities:
             hispanic_state_eth = hispanic_state[hispanic_state['RACE']==eth] #All the Hispanic ethnicities will be summed
-            hispanic_state_eth = hispanic_state.reset_index()
-            for age in age_groups:
-                hispanic_state_eth_age = np.sum(hispanic_state_eth.loc[age,'POPESTIMATE2019'])
-                pdb.set_trace()
-def vis_states(epidemic_data, outdir):
+            hispanic_state_eth = hispanic_state_eth.reset_index()
+            #First
+            age_counts[0]+=np.sum(hispanic_state_eth.loc[0,'POPESTIMATE2019'])
+            for i in range(len(age_index)-1):
+                age_counts[i+1]+=np.sum(hispanic_state_eth.loc[age_index[i]:age_index[i+1],'POPESTIMATE2019'])
+            #Last
+            age_counts[-1]+=np.sum(hispanic_state_eth.loc[85:,'POPESTIMATE2019'])
+        vals.extend(age_counts)
+        slice = pd.DataFrame([vals],columns=extracted_data.columns)
+        extracted_data=pd.concat([extracted_data,slice])
+
+        #All the ethnicities in the non Hispanic should not be summed
+        #Loop through origins
+        non_hispanic_state = per_state[per_state['ORIGIN']==1]
+        #All the ethnicities in Hispanic should be summed
+        for eth in ethnicities:
+            vals = []
+            vals.append(state)
+            vals.append('Non-Hispanic '+ethnicities[eth])
+            age_counts = np.zeros(len(age_groups))
+
+            non_hispanic_state_eth = non_hispanic_state[non_hispanic_state['RACE']==eth] #All the Hispanic ethnicities will be summed
+            non_hispanic_state_eth =non_hispanic_state_eth.reset_index()
+            #First
+            age_counts[0]+=np.sum(non_hispanic_state_eth.loc[0,'POPESTIMATE2019'])
+            for i in range(len(age_index)-1):
+                age_counts[i+1]+=np.sum(non_hispanic_state_eth.loc[age_index[i]:age_index[i+1],'POPESTIMATE2019'])
+            #Last
+            age_counts[-1]+=np.sum(non_hispanic_state_eth.loc[85:,'POPESTIMATE2019'])
+            vals.extend(age_counts)
+            slice = pd.DataFrame([vals],columns=extracted_data.columns)
+            extracted_data=pd.concat([extracted_data,slice])
+
+    #Save df
+    extracted_data.to_csv('formatted_eth_age_data_per_state.csv')
+    return extracted_data
+
+def vis_states(epidemic_data, eth_age_data_per_state, outdir):
     '''Plot the deaths per state and feature
     '''
-
-    states = epidemic_data['State'].unique()
+    #Handle nans
+    epidemic_data = epidemic_data.fillna(0)
+    states = eth_age_data_per_state['State'].unique()
     for state in states:
         fig, ax = plt.subplots(figsize=(18/2.54, 12/2.54))
-        state_data = epidemic_data[epidemic_data['State']==state]
-        ethnicities = state_data['Race and Hispanic Origin Group'].unique()
-        age_groups = state_data['Age group'].unique()
+        epi_state_data = epidemic_data[epidemic_data['State']==state]
+        eth_age_state_data = eth_age_data_per_state[eth_age_data_per_state['State']==state]
+        ethnicities = epi_state_data['Race and Hispanic Origin Group'].unique()[:-1]
+        age_groups = epi_state_data['Age group'].unique()
         prev=np.zeros(len(age_groups))
         for ethnicity in ethnicities:
-            eth_state_data = state_data[state_data['Race and Hispanic Origin Group']==ethnicity]
-            if np.sum(prev)<1:
-                ax.bar(eth_state_data['Age group'], eth_state_data['COVID-19 Deaths'], label=ethnicity)
+
+            eth_state_data = epi_state_data[epi_state_data['Race and Hispanic Origin Group']==ethnicity]
+            #Normalize the deaths with the population distribution per ethnicity
+            norm_vals = eth_age_state_data[eth_age_state_data['Ethnicity']==ethnicity]
+            if ethnicity=='Unknown':
+                continue
             else:
-                ax.bar(eth_state_data['Age group'], eth_state_data['COVID-19 Deaths'],bottom = prev, label=ethnicity)
-            prev += eth_state_data['COVID-19 Deaths']
+                normalized = (eth_state_data['COVID-19 Deaths'].values/norm_vals[eth_state_data['Age group']].values)[0]
+                normalized = normalized*100
+
+            if np.sum(prev)<1:
+                ax.bar(eth_state_data['Age group'],normalized, label=ethnicity)
+            else:
+                ax.bar(eth_state_data['Age group'], normalized, bottom = prev, label=ethnicity)
+            prev += normalized
+
+        if state == 'Indiana':
+            pdb.set_trace()
         plt.xticks(rotation='vertical')
         plt.legend()
         ax.set_title(state)
-        ax.set_ylabel('Deaths')
+        ax.set_ylabel('Death fraction per ethnicity')
         fig.tight_layout()
         fig.savefig(outdir+state.replace(" ", "_")+'.png', format='png')
         plt.close()
@@ -109,7 +165,12 @@ epidemic_data = pd.read_csv(args.epidemic_data[0])
 comorbidity_data = pd.read_csv(args.comorbidity_data[0])
 sex_eth_age_data = pd.read_csv(args.sex_eth_age_data[0])
 outdir = args.outdir[0]
-#vis_states(epidemic_data, outdir)
+#Format age counts data per ethnicity
+try:
+    eth_age_data_per_state = pd.read_csv('formatted_eth_age_data_per_state.csv')
+except:
+    eth_age_data_per_state = format_age_per_ethnicity(sex_eth_age_data)
+
+vis_states(epidemic_data, eth_age_data_per_state, outdir)
 #vis_comorbidity(comorbidity_data, comorbidity_data['Condition'].unique()[:1], outdir+'comorbidity/comorbidity1.png')
 #vis_comorbidity(comorbidity_data, comorbidity_data['Condition'].unique()[1:], outdir+'comorbidity/comorbidity2.png')
-format_ethnicity(sex_eth_age_data)
