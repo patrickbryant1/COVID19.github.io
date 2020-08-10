1c28fd929d087e983d037701e122de0bec6ff861
Patrick <patrick.bryant@live.com>
2020-08-10 14:25:31 +0200

    ficing labels

--- simulations/mobility/model_output/R0_2_79/3_week_forecast/19_April/plots/montage.sh
+++ simulations/mobility/model_output/R0_2_79/3_week_forecast/19_April/plots/montage.sh
@@ -30,14 +30,14 @@ montage 'correlations/retail_and_recreation_percent_change_from_baseline.png' 'c
 #Montage EpiEstim and mobility R estimate comparison
 montage "EpiEstim/Austria.png" "EpiEstim/Belgium.png" "EpiEstim/Denmark.png" "EpiEstim/France.png" "EpiEstim/Germany.png" "EpiEstim/Italy.png" "EpiEstim/Norway.png" "EpiEstim/Spain.png" "EpiEstim/Sweden.png" "EpiEstim/Switzerland.png" "EpiEstim/United_Kingdom.png" -tile 3x4 -geometry +2+2 EpiEstim/all.png
 
-#Montage posterior distributions for LOO analysis
-montage "LOO/Austria_0.png" "LOO/Belgium_0.png" "LOO/Denmark_0.png" "LOO/France_0.png" "LOO/Germany_0.png" "LOO/Italy_0.png" "LOO/Norway_0.png" "LOO/Spain_0.png" "LOO/Sweden_0.png" "LOO/Switzerland_0.png" "LOO/United_Kingdom_0.png" -tile 6x2 -geometry +2+2 LOO/all_retail.png
-montage "LOO/Austria_1.png" "LOO/Belgium_1.png" "LOO/Denmark_1.png" "LOO/France_1.png" "LOO/Germany_1.png" "LOO/Italy_1.png" "LOO/Norway_1.png" "LOO/Spain_1.png" "LOO/Sweden_1.png" "LOO/Switzerland_1.png" "LOO/United_Kingdom_1.png" -tile 6x2 -geometry +2+2 LOO/all_grocery.png
-montage "LOO/Austria_2.png" "LOO/Belgium_2.png" "LOO/Denmark_2.png" "LOO/France_2.png" "LOO/Germany_2.png" "LOO/Italy_2.png" "LOO/Norway_2.png" "LOO/Spain_2.png" "LOO/Sweden_2.png" "LOO/Switzerland_2.png" "LOO/United_Kingdom_2.png" -tile 6x2 -geometry +2+2 LOO/all_transit.png
-montage "LOO/Austria_3.png" "LOO/Belgium_3.png" "LOO/Denmark_3.png" "LOO/France_3.png" "LOO/Germany_3.png" "LOO/Italy_3.png" "LOO/Norway_3.png" "LOO/Spain_3.png" "LOO/Sweden_3.png" "LOO/Switzerland_3.png" "LOO/United_Kingdom_3.png" -tile 6x3 -geometry +3+3 LOO/all_work.png
-montage "LOO/Austria_4.png" "LOO/Belgium_4.png" "LOO/Denmark_4.png" "LOO/France_4.png" "LOO/Germany_4.png" "LOO/Italy_4.png" "LOO/Norway_4.png" "LOO/Spain_4.png" "LOO/Sweden_4.png" "LOO/Switzerland_4.png" "LOO/United_Kingdom_4.png" -tile 6x4 -geometry +4+4 LOO/all_residential.png
-montage LOO/all_retail.png LOO/all_grocery.png LOO/all_transit.png LOO/all_work.png LOO/all_residential.png -tile 1x5 -geometry +2+2 LOO/all_mob.png
-
 #Montage correlations for LOO analysis
 #Figure S1
 montage  "LOO/Austria_Rt_corr.png"  "LOO/Belgium_Rt_corr.png" "LOO/Denmark_Rt_corr.png" "LOO/France_Rt_corr.png" "LOO/Germany_Rt_corr.png" "LOO/Italy_Rt_corr.png" "LOO/Norway_Rt_corr.png" "LOO/Spain_Rt_corr.png" "LOO/Sweden_Rt_corr.png" "LOO/Switzerland_Rt_corr.png" "LOO/United_Kingdom_Rt_corr.png" -tile 3x4 -geometry +2+2 '/home/patrick/COVID19.github.io/simulations/mobility/publication_figures/supplementary/FigureS1.png'
+
+#Figure S5
+BASE=/home/patrick/COVID19.github.io/simulations/icl_model/model_output/3_week_forecast/plots/forecast
+montage $BASE/Austria_forecast.png  $BASE/Germany_forecast.png  $BASE/Sweden_forecast.png  $BASE/Belgium_forecast.png  $BASE/Italy_forecast.png $BASE/Switzerland_forecast.png $BASE/Denmark_forecast.png  $BASE/Norway_forecast.png  $BASE/United_Kingdom_forecast.png $BASE/France_forecast.png  $BASE/Spain_forecast.png -tile 3x4 -geometry +2+2 '/home/patrick/COVID19.github.io/simulations/mobility/publication_figures/supplementary/FigureS5.png'
+
+#Figure S6
+#Montage posterior distributions for LOO analysis
+montage LOO/all_retail.png LOO/all_grocery.png LOO/all_transit.png LOO/all_work.png LOO/all_residential.png -tile 1x5 -geometry +2+2 /home/patrick/COVID19.github.io/simulations/mobility/publication_figures/supplementary/FigureS6.png
